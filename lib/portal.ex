9fe892d3eeeb6b33ed23824eda9fd91f6609c24d
Michelle Dotzenrod <mdotzenrod@academicworks>
2016-07-18 22:11:52 -0500

    initial commit Portal project

---
+++ lib/portal.ex
@@ -0,0 +1,52 @@
+defmodule Portal do
+  use Application
+
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    # Define workers and child supervisors to be supervised
+    children = [
+      # Starts a worker by calling: Portal.Worker.start_link(arg1, arg2, arg3)
+      # worker(Portal.Worker, [arg1, arg2, arg3]),
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Portal.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  defstruct[:left, :right]
+  @doc """
+  Starts transfering `data` from `left` to `right`.
+  """
+
+    def transfer(left, right, data) do
+      #First add all the data to the portal on the left
+      for item <- data do
+        Portal.Door.push(left, item)
+      end
+    end
+
+    #Returns a portal struct we will use next
+    %Portal{left: left, right: right}
+  end
+
+  @doc """
+  Pushes data to the right in the given `portal`.
+  """
+  def push_right(portal) do
+    # See i we can pop data from left. If so, push the
+    # popped data to the right. Otherwise, do nothing.
+    case Portal.Door.pop(portal.left) do
+      :error   -> :ok
+      {:ok, h} -> Portal.Door.push(portal.right, h)
+    end
+
+    # Return the portal itself
+    portal
+  end
+
+end
